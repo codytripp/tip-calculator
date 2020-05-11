1d8828de8b83270123743e319241fca904e6c49c
Cody Tripp <codytripp@cdt.local>
2020-05-11 15:35:02 -0700

    added html,css, and js scripts for tip calc

---
+++ tipcalc.js
@@ -0,0 +1,32 @@
+//Calculate Tip
+function calculateTip() {
+   const billAmt = document.getElementById("billamt").value;
+   const servQual = document.getElementById("serviceQual").value;
+   const numPeople = document.getElementById("peopleamt").value;
+  
+    //validate input
+    if(billAmt === "" || servQual == 0){
+        alert("Please enter values.");
+        return;
+    }
+    
+    //Check to see if this input is empty or less than or equal to 1
+    
+  
+    //Calculate tip
+    
+    //round to two decimal places
+    
+    //next line allows us to always have two digits after decimal point
+    
+    //Display the tip
+    
+  
+  
+  
+  //Hide the tip amount on load
+  
+  
+  //click to call function
+} 
+  
