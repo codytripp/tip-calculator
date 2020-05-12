9db11e33bc776e463af31fb2b9c23f92972a3ff7
Cody Tripp <codytripp@cdt.local>
2020-05-12 10:47:38 -0700

    finished JS files for functioning calculator

--- tipcalc.js
+++ tipcalc.js
@@ -1,32 +1,40 @@
 //Calculate Tip
 function calculateTip() {
-   const billAmt = document.getElementById("billamt").value;
-   const servQual = document.getElementById("serviceQual").value;
-   const numPeople = document.getElementById("peopleamt").value;
-  
-    //validate input
-    if(billAmt === "" || servQual == 0){
-        alert("Please enter values.");
-        return;
-    }
-    
-    //Check to see if this input is empty or less than or equal to 1
-    
-  
-    //Calculate tip
-    
-    //round to two decimal places
-    
-    //next line allows us to always have two digits after decimal point
-    
-    //Display the tip
-    
-  
-  
-  
-  //Hide the tip amount on load
-  
-  
-  //click to call function
-} 
-  
+  var billAmt = document.getElementById("billamt").value;
+  var serviceQual = document.getElementById("serviceQual").value;
+  var numOfPeople = document.getElementById("peopleamt").value;
+
+  //validate input
+  if (billAmt === "" || serviceQual == 0) {
+    alert("Please enter values");
+    return;
+  }
+  //Check to see if this input is empty or less than or equal to 1
+  if (numOfPeople === "" || numOfPeople <= 1) {
+    numOfPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  //Calculate tip
+  var total = (billAmt * serviceQual) / numOfPeople;
+  //round to two decimal places
+  total = Math.round(total * 100) / 100;
+  //next line allows us to always have two digits after decimal point
+  total = total.toFixed(2);
+  //Display the tip
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+
+}
+
+//Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//click to call function
+document.getElementById("calculate").onclick = function() {
+  calculateTip();
+
+};
